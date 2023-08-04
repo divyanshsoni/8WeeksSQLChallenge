e6ecdf1109d541cf9d805da0a8cf6f2fd067eb8b
Divyansh Soni <35307737+divyanshsoni@users.noreply.github.com>
2023-08-04 10:51:36 +0530

    Update Data Mart.sql

--- Data Mart.sql
+++ Data Mart.sql
@@ -25,4 +25,31 @@ round(sales::numeric/transactions,2) as avg_transactions
 from data_cte);
 
 -- Data Exploration
-1
+-- 1. What day of the week is used for each week_date value?
+select distinct to_char(formatted_date, 'Day') as distinct_day
+from data_mart.clean_weekly_sal
+
+-- 2. What range of week numbers are missing from the dataset?
+select distinct week_number as number
+from data_mart.clean_weekly_sales
+order by week_number
+
+-- 3. How many total transactions were there for each year in the dataset?
+select calendar_year, count(avg_transactions) as total_transactions
+from data_mart.clean_weekly_sales
+group by calendar_year
+order by calendar_year
+
+-- 4. What is the total sales for each region for each month?
+select region, month_number, sum(sales) as total_sales
+from data_mart.clean_weekly_sales
+group by region, month_number
+order by region, month_number
+
+-- 5. What is the total count of transactions for each platform
+select platform, count(avg_transactions) as transaction_count
+from data_mart.clean_weekly_sales
+group by platform
+order by platform
+
+-- 6. 
